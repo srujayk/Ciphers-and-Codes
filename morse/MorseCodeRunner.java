38ef945c5652a372dfa241814dde603f36451bdc
Srujay Korlakunta <srujayk@gmail.com>
2016-08-16 12:11:42 -0500

    Added vigenere and morse files.

---
+++ morse/MorseCodeRunner.java
@@ -0,0 +1,34 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab07e
+{
+	public static void main( String args[] )
+	{
+		MorseCode test = new MorseCode('A');
+		out.println (test);
+
+		test = new MorseCode('B');
+		out.println (test);
+
+		test = new MorseCode('3');
+		out.println (test);
+
+		test = new MorseCode('Z');
+		out.println (test);
+
+		test = new MorseCode('8');
+		out.println (test);
+
+		test = new MorseCode('F');
+		out.println (test);
+
+		test = new MorseCode('0');
+		out.println (test);
+	}
+}
